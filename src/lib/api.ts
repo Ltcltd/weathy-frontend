b7a61b8f56fad06f2795f2fb5c4b28f209d77dfd
Mohit Mishra <libremelon@proton.me>
2025-10-09 01:13:04 +0530

    update baseurl

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,5 @@
 // API Configuration
-export const API_BASE_URL = "http://buffnux-vm.southindia.cloudapp.azure.com";
+export const API_BASE_URL = "https://api.weathy.earth";
 
 // API Endpoints
 export const API_ENDPOINTS = {
